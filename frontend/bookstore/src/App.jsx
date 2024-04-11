8e088fd6fe4cd91e070a3675abf8cb1807128254
arohidsilva <arohidsilva311@gmail.com>
2024-04-12 00:50:39 +0530

    Created Memberinfo page

--- frontend/bookstore/src/App.jsx
+++ frontend/bookstore/src/App.jsx
@@ -2,8 +2,13 @@ import './App.css'
 import {createBrowserRouter , RouterProvider} from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Login from './components/Login';
+import Memberinfo from './components/Memberinfo';
 
 const router = createBrowserRouter([
+  {
+    path: "/Memberinfo/",
+    element: <Memberinfo />
+  },
   {
     path: "/Login/",
     element: <Login />
---
+++ frontend/bookstore/src/components/Memberinfo.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import "../stylesheets/Memberinfo.css"
+import React from 'react';
+
+function Memberinfo() {
+  return (
+    <div className="MIHeader">
+        <h2>Member Info</h2>
+        
+    </div>
+  )
+}
+
+export default Memberinfo
