10a6c83dae90a0b1082b73fde0e3e4b37d84e1e2
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-04-11 23:58:39 +0530

    Book List UI #1

--- frontend/bookstore/src/App.jsx
+++ frontend/bookstore/src/App.jsx
@@ -1,26 +1,24 @@
-import './App.css'
-import {createBrowserRouter , RouterProvider} from 'react-router-dom';
-import Homepage from './components/Homepage';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Books from "./components/Books";
 
 const router = createBrowserRouter([
   {
-    path : "/home" ,
-    element : <Homepage /> ,
-    
-  } ,
-  {
-    path : "/" ,
-    element : <Homepage /> ,
-    
+    path: "/home",
+    element: <Homepage />,
   },
-])
+  {
+    path: "/books",
+    element: <Books />,
+  },
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
 function App() {
-  
-  
-
-  return (
-    <RouterProvider router={router} />  
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- frontend/bookstore/src/components/Books.jsx
+++ frontend/bookstore/src/components/Books.jsx
@@ -1,9 +1,41 @@
 // import React from 'react'
 
 function Books() {
+  let bookData = [0, 0, 0, 0, 0, 0, 0];
+
   return (
-    <div>Books</div>
-  )
+    <div className="container">
+      <div className="card-deck mb-3 text-center row">
+        {bookData.map((book, bookID) => {
+          return (
+            <div key={bookID} className="p-1 col-md-4 ">
+              <div className="m-1 box-shadow card p-0">
+                <div className="card-header">
+                  <h4 className="my-0 font-weight-normal">#012343345</h4>
+                </div>
+                <div className="card-body">
+                  <h4 className="text-truncate">Harry Potter and the prisoner of Azkaban</h4>
+                  <div className="d-flex w-100">
+                    <div className="w-25 text-start">Author</div>
+                    <div className="w-75 text-start text-truncate">J K Rowling</div>
+                  </div>
+                  <div className="d-flex w-100 mt-2">
+                    <div className="w-75 d-flex  text-start">
+                    <div className="btn btn-primary mx-2 p-1 ">Fantasy</div>
+                    <div className="btn btn-primary mx-2 p-1">Adventure</div>
+                    </div>
+                    <div className="w-25 text-end">
+                      <div className="btn btn-success">13</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Books
+export default Books;
