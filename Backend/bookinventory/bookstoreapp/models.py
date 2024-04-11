b87b69bdcb60827cd9e27092eee5d61951613db8
Ashish Achar <ashishachar96@gmail.com>
2024-04-12 00:07:05 +0530

    Settings.py and Models.py is updated

--- Backend/bookinventory/bookstoreapp/models.py
+++ Backend/bookinventory/bookstoreapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Logins(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_password = models.CharField(max_length=12) 
+
+class BookDetails(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=250)
+    no_of_copies = models.IntegerField()
+
+class Category(models.Model):
+    book_no =models.ForeignKey("BookDetails", on_delete=models.CASCADE)
+    categ = models.CharField(max_length=30)
+
+class Members(models.Model):
+    name = models.CharField(max_length=50)
+    email_id = models.EmailField(max_length=254)
+    contact_no = models.IntegerField()
+    penalty = models.DecimalField(max_digits=4, decimal_places=2)
+
+class Transaction(models.Model):
+    memb_id = models.ForeignKey("Members", on_delete=models.CASCADE)
+    bookid = models.ForeignKey("BookDetails", on_delete=models.CASCADE)
+    borrow_date = models.DateField()
+    return_date = models.DateField()
+    status = models.BooleanField()
