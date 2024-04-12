4af5af9a9f0255a534b16e36cbe1d7e5d370ff74
arohidsilva <arohidsilva311@gmail.com>
2024-04-12 09:01:33 +0530

    UI changes done to the Login

--- frontend/bookstore/src/components/Memberinfo.jsx
+++ frontend/bookstore/src/components/Memberinfo.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
-import "../stylesheets/Memberinfo.css"
+import "../stylesheets/Memberinfo.css";
 import React from 'react';
+import { IoMdAddCircle } from "react-icons/io";
 
 function Memberinfo() {
   return (
@@ -8,6 +9,10 @@ function Memberinfo() {
         <div className="MIHeader">
         <h2>Members&#39; Info</h2>
         </div>
+        <div className="MIAddContainer">
+            <div className="MIAddBtn"><IoMdAddCircle className="MIAddIcon" />&nbsp;Add Member</div>
+        </div>
+        
     </div>
     
   )
