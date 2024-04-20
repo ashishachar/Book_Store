d085d5fe263961c0cd4bddcbcb628861627d2606
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-04-21 01:25:13 +0530

    Login page API call changes

--- frontend/bookstore/src/components/Login.jsx
+++ frontend/bookstore/src/components/Login.jsx
@@ -1,8 +1,33 @@
 /* eslint-disable no-unused-vars */
 import "../stylesheets/Login.css"
 import React from 'react';
+import { axios , dbURL  } from "../utils/api-calls";
+import {useState , useEffect} from 'react';
+import {redirect, useNavigate} from 'react-router-dom';
 
 function Login() {
+  const [info , setInfo] = useState({user : '' , password : ''});
+  const navigate = useNavigate();
+  const onSubmitMeth =()=>{
+    console.log('Loggin in : ',info);
+    const loginMeth = async ()=>{
+      await axios.post(dbURL+"/admins/" , info).then(res=>{
+        // console.log(res.data.state);
+        if(res && res.data.state)
+        {
+          navigate('/home');
+        }
+        else{
+          navigate('/login');
+        }
+      })
+      .catch(err=>{
+        // console.log(err);
+      })
+      // console.log(data);
+    }
+    loginMeth();
+  }
   return (
     <div className="LIContent1">
         <div className="LIOrgTitle"></div>
@@ -10,9 +35,11 @@ function Login() {
             <div className="LIHeader">ABC Book Store</div>
             <div className="LIContent3">
                 <p>Login</p>
-                <input className="LIUserName" type="text" placeholder="Enter User Name"></input><br/>
-                <input className="LIUserName" type="password" placeholder="Enter Password"></input><br/>
-                <button className="LILoginBtn">Login</button>
+                <input className="LIUserName" type="text" placeholder="Enter User Name" value={info.user} 
+                onChange={(e)=>{setInfo({...info , user : e.target.value})}}></input><br/>
+                <input className="LIUserName" type="password" placeholder="Enter Password" value={info.password}
+                onChange={(e)=>{setInfo({...info , password : e.target.value})}}></input><br/>
+                <button className="LILoginBtn" onClick={onSubmitMeth}>Login</button>
             </div>
         </div>
     </div>
